6b6138a6a72f8fa40eb0fbe628769195a7e51ac5
ccordenier <christophe.cordenier@gmail.com>
2011-06-29 22:33:54 +0200

    Add hornet listener component

--- src/main/java/com/nectify/tapestry/hornet/components/HornetListener.java
+++ src/main/java/com/nectify/tapestry/hornet/components/HornetListener.java
@@ -15,7 +15,7 @@ import com.nectify.tapestry.hornet.services.HornetSymbols;
  * @author ccordenier
  * 
  */
-public class InitHornet {
+public class HornetListener {
 
 	@Inject
 	@Symbol(HornetSymbols.HORNET_HOST)
@@ -31,26 +31,47 @@ public class InitHornet {
 	@Inject
 	private Hornet hornet;
 
+	/**
+	 * The name of channel to open a connection with.
+	 */
 	@Parameter(required = true)
 	private String channel;
 
+	/**
+	 * The event to listen to.
+	 */
+	@Parameter(required = true)
+	private String event;
+
+	/**
+	 * The javascript handler method to callback on event.
+	 */
+	@Parameter(required = true)
+	private String handler;
+
+	/**
+	 * Add JS imports.
+	 * 
+	 * @param writer
+	 */
 	void beginRender(MarkupWriter writer) {
-		writer
-				.writeRaw(String
-						.format(
-								"<script src=\"http://%s:%s/socket.io/socket.io.js\"></script>",
-								host, port));
-		writer.writeRaw(String.format(
-				"<script src=\"http://%s:%s/hornet/hornet.js\"></script>",
-				host, port));
+		support.importJavaScriptLibrary(String.format(
+				"http://%s:%s/socket.io/socket.io.js", host, port));
+		support.importJavaScriptLibrary(String.format(
+				"http://%s:%s/hornet/hornet.js", host, port));
 	}
 
+	/**
+	 * Init hornet client.
+	 * 
+	 * @param writer
+	 */
 	void afterRender(MarkupWriter writer) {
+		String hornetVar = support.allocateClientId("hornet");
 		String token = hornet.createAccessToken(channel);
-		support
-				.addScript(
-						"var hornet = new Hornet('%s', %s, '%s', '%s');hornet.connect();",
-						host, port, channel, token);
+		support.addScript(
+				"var %s = new Hornet('%s', %s, '%s', '%s');hornet.connect();",
+				hornetVar, host, port, channel, token);
+		support.addScript("%s.on('%s', %s);", hornetVar, event, handler);
 	}
-
 }
